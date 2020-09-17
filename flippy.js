8e5d83eaa75b258b3b8c8f49d3486f16b7ec3bf0
ste <foalestephen@gmail.com>
2020-09-17 13:49:12 +0100

    new services section

--- flippy.js
+++ flippy.js
@@ -3,3 +3,11 @@ $('.flip').hover(function(){
         $(this).find('.card').toggleClass('flipped');
 
  });
+
+$(document).ready(function() {
+
+  $('.flipper').hover(function () {
+    $(this).find('.flipper-card').toggleClass('flipper-is-flipped');
+  });
+
+});
--- scrolling.js
+++ scrolling.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
       // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 900, function(){
+      }, 600, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
